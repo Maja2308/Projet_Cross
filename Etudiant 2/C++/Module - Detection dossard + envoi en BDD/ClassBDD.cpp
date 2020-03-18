6879ca596135ac081eb0bfa3b3dce2c965667d09
Maja2308 <matthias.jouen23@gmail.com>
2020-03-18 15:36:49 +0100

    C++ avancement bdd et module

---
+++ Etudiant 2/C++/Module - Detection dossard + envoi en BDD/ClassBDD.cpp
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "ClassBDD.h"
+#include <windows.h>
+#include <tchar.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
